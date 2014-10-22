0b5800479fdf10e13605bd1d92bcebc5e0f49c0e
sesto <sesto26@hotmail.com>
2014-10-22 22:26:27 +0200

    refactored for yeoman

--- src/main/webapp/js/services.js
+++
@@ -1,5 +0,0 @@
-var fileServices = angular.module('fileServices', [ 'ngResource' ]);
-
-fileServices.factory('FileService', [ '$resource', function($resource) {
-	return $resource('/homearchive/rs/findFiles/:id', {});
-} ]);
--- yo/app/scripts/services/services.js
+++ yo/app/scripts/services/services.js
@@ -1,5 +1,10 @@
 var fileServices = angular.module('fileServices', [ 'ngResource' ]);
 
 fileServices.factory('FileService', [ '$resource', function($resource) {
-	return $resource('/homearchive/rs/findFiles/:id', {});
+	return $resource('http://localhost:8080/homearchive/rs/findFiles/:id', {id:'@id'},
+			{
+		update:{
+			method:'PUT'
+		}
+			});
 } ]);
