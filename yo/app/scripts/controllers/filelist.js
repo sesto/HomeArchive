32c22eda760b2288f70db10efd180a48ac367c9e
sesto <sesto26@hotmail.com>
2014-10-28 09:38:58 +0100

    added code for sorting and filtering

--- yo/app/scripts/controllers/filelist.js
+++ yo/app/scripts/controllers/filelist.js
@@ -5,122 +5,149 @@
  * @name homearchiveApp.controller:FilelistCtrl
  * @description # FilelistCtrl Controller of the homearchiveApp
  */
-angular.module('homearchiveApp').controller(
-		'FileListCtrl',
-		[
-				'$scope',
-				'FileService',
-				'ngTableParams',
-				function($scope, FileService, ngTableParams) {
-					$scope.files = [];
-					$scope.file = {};
-					$scope.file.tags = [];
+angular
+		.module('homearchiveApp')
+		.controller(
+				'FileListCtrl',
+				[
+						'$scope',
+						'$filter',
+						'FileService',
+						'ngTableParams',
+						function($scope, $filter, FileService, ngTableParams) {
+							$scope.files = [];
+							$scope.file = {};
+							$scope.file.tags = [];
+							var data;
 
-					$scope.addTag = function() {
-						$scope.file.tags.push($scope.tag);
-						$scope.tag = '';
-						$scope.removeTag = function(index) {
-							$scope.file.tags.splice(index, 1);
+							$scope.addTag = function() {
+								$scope.file.tags.push($scope.tag);
+								$scope.tag = '';
+								$scope.removeTag = function(index) {
+									$scope.file.tags.splice(index, 1);
 
-						}
-					};
-					var scopeFile;
-					var clean;
-					$scope.tableParams = new ngTableParams({
-						page : 1, // show first page
-						count : 10
-					// count per page
-					}, {
-						total : $scope.files.length, // length of data
-						getData : function($defer, params) {
-							params.total($scope.files.length);
-							$defer.resolve($scope.files.slice(
-									(params.page() - 1) * params.count(),
-									params.page() * params.count()));
-						}
+								}
+							};
+							var scopeFile;
+							var clean;
+							$scope.tableParams = new ngTableParams(
+									{
+										page : 1, // show first page
+										count : 10,
+										// count per page
+										sorting : {
+											docDate: "desc"// initial sorting
+										}
+									},
+									{
+										total : $scope.files.length, // length
+																		// of
+																		// data
+										getData : function($defer, params) {
 
-					});
+											var filteredData = params.filter() ? $filter(
+													'filter')($scope.files,
+													params.filter())
+													: $scope.files;
 
-					// does search with parameters
-					$scope.submit = function() {
-						console.log($scope.file.tags);
-						var queryParams = {
-							'params' : {
-								fileName : $scope.file.fileName,
-								startDate : $scope.file.startDate,
-								endDate : $scope.file.endDate,
-								tags : $scope.file.tags
-							}
-						};
+											var orderedData = params.sorting() ? $filter(
+													'orderBy')(filteredData,
+													params.orderBy())
+													: $scope.files;
 
-						FileService.query(queryParams).$promise.then(function(
-								data) {
-							$scope.files = data;
-							console.log(JSON.stringify($scope.files));
-							$scope.getTag = function(file) {
-								return angular.isArray(file.tags) ? file.tags
-										: [ file.tags ];
+											params.total(orderedData.length);
+											$defer.resolve(orderedData.slice(
+													(params.page() - 1)
+															* params.count(),
+													params.page()
+															* params.count()));
+										}
+									});
+
+							// does search with parameters
+							$scope.submit = function() {
+								console.log($scope.file.tags);
+								var queryParams = {
+									'params' : {
+										fileName : $scope.file.fileName,
+										startDate : $scope.file.startDate,
+										endDate : $scope.file.endDate,
+										tags : $scope.file.tags
+									}
+								};
+
+								FileService.query(queryParams).$promise
+										.then(function(data) {
+											$scope.files = data;
+											console.log(JSON
+													.stringify($scope.files));
+											$scope.getTag = function(file) {
+												return angular
+														.isArray(file.tags) ? file.tags
+														: [ file.tags ];
+											};
+
+											$scope.tableParams.reload();
+										});
 							};
 
-							$scope.tableParams.reload();
-						});
-					};
+							// clears the form
+							clean = function() {
+								$scope.file.fileName = null;
+								$scope.file.tags = [];
+								$scope.file.startDate = null;
+								$scope.file.endDate = null;
+							};
+							$scope.clear = function() {
+								clean();
+							}
 
-					// clears the form
-					clean = function() {
-						$scope.file.fileName = null;
-						$scope.file.tags = [];
-						$scope.file.startDate = null;
-						$scope.file.endDate = null;
-					};
-					$scope.clear = function() {
-						clean();
-					}
+							// deletes file
+							$scope.remove = function(file) {
+								if (confirm("Are you sure you want to delete "
+										+ file.fileName + ' from the database?') == false) {
+									return;
+								} else {
+									console.log('Removing file with id: '
+											+ file.id);
+									FileService.remove({
+										id : file.id
+									}).$promise.then(function() {
+										var idx = $scope.files.indexOf(file);
+										$scope.files.splice(idx, 1);
+										console.log('removed from scope, idx: '
+												+ $scope.files.indexOf(file));
+										$scope.tableParams.reload();
+									});
+								}
+								;
+							};
 
-					// deletes file
-					$scope.remove = function(file) {
-						if (confirm("Are you sure you want to delete "
-								+ file.fileName + ' from the database?') == false) {
-							return;
-						} else {
-							console.log('Removing file with id: ' + file.id);
-							FileService.remove({
-								id : file.id
-							}).$promise.then(function() {
-								var idx = $scope.files.indexOf(file);
-								$scope.files.splice(idx, 1);
-								console.log('removed from scope, idx: '
-										+ $scope.files.indexOf(file));
-								$scope.tableParams.reload();
-							});
-						};
-					};
+							// pushes values into the model for editing
+							$scope.edit = function(file) {
+								$scope.editStatus = true;
+								$scope.file.fileName = file.fileName;
+								$scope.file.tags = file.tags;
+								scopeFile = file;
 
-					// pushes values into the model for editing
-					$scope.edit = function(file) {
-						$scope.editStatus = true;
-						$scope.file.fileName = file.fileName;
-						$scope.file.tags = file.tags;
-						scopeFile = file;
+							};
 
-					};
+							// submits updated values to the service
+							$scope.update = function() {
+								FileService.update({
+									id : scopeFile.id
+								}, {
+									fileName : $scope.file.fileName,
+									tags : $scope.file.tags
+								}
 
-					// submits updated values to the service
-					$scope.update = function() {
-						FileService.update({
-							id : scopeFile.id
-						}, {
-							fileName : $scope.file.fileName,
-							tags : $scope.file.tags
-						}
+								).$promise.then(function() {
+									$scope.editStatus = false;
+									alert('File is updated');
+									clean();
+								}
 
-						).$promise.then(function() {
-							$scope.editStatus = false;
-							alert('File is updated');
-							clean();
-						}
+								);
+							};
 
-						);
-					};
-
-				} ]);
+						} ]);
